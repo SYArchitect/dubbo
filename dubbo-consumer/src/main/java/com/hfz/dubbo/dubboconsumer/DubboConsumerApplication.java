e714168a476169ddfd6ad8a1c1a4c3618ab3a6b0
Huangfuzhi <1310209619@qq.com>
2019-10-05 22:20:55 +0800

    项目初始化

---
+++ dubbo-consumer/src/main/java/com/hfz/dubbo/dubboconsumer/DubboConsumerApplication.java
@@ -0,0 +1,17 @@
+package com.hfz.dubbo.dubboconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HFZ
+ */
+
+@SpringBootApplication
+public class DubboConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboConsumerApplication.class, args);
+    }
+
+}
